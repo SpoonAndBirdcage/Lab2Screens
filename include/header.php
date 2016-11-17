0716542d7482e0d7f8e3d33499012d773e62acb9
Spoon And Birdcage <spoonandbirdcage@gmail.com>
2016-11-17 14:48:12 -0700

    Initial Framework

---
+++ include/header.php
@@ -0,0 +1,14 @@
+<header>
+		<h1>Nait Dental School</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Patients</a></li>
+				<li><a href="#">Students</a></li>
+				<li><a href="#">Instructors</a></li>
+				<li><a href="#">Class</a></li>
+				<li><a href="#">Rooms</a></li>
+				<li><a href="#">Appointments</a></li>
+				<li><a href="#">Inventory</a></li>
+			</ul>
+		</nav>
+	</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Nait Dental School</title>
+</head>
+<body>
+	<?php include('/include/header.php') ?>
+	<main>
+		
+	</main>
+	<?php include('/include/footer.php') ?>
+</body>
+</html>
