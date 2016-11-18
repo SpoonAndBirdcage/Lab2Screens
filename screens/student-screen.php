0f0387ca7613bdd9883a20f12df12ea7707cd1bd
ExoBuyser <ebuyser1@studentmail.nait.ca>
2016-11-18 12:00:02 -0700

    Student-screen

---
+++ screens/student-screen.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		form p{
+			margin: 0; 
+			clear: left; 
+			font-size: 20px;
+			margin-left: 20px;
+			font-family: 'Vollkorn', serif;
+		}
+		form label{
+			float: left; 
+			width: 200px; 
+		}
+
+	</style>
+</head>
+<body>
+	<form name="" method="">
+		<h1>Register Student</h1>
+		<p><label>First Name:</label>
+		<input type="" name=""></p> 
+
+		<p><label>Last Name:</label>
+		<input type="" name=""></p> 
+
+		<p><label>Street Address:</label>
+		<input type="" name=""></p> 
+
+		<p><label>Postal Code:</label>
+		<input type="" name=""></p> 
+
+		<p><label>City:</label> 
+		<select>
+			<option value="">Edmonton</option>
+			<option value="">Calgary</option>
+		</select></p>
+
+		<p><label>Province:</label>
+		<select>
+			<option value="AB">Alberta</option>
+			<option value="BC">British Columbia</option>
+			<option value="MB">Manitoba</option>
+			<option value="NB">New Brunswick</option>
+			<option value="NL">Newfoundland and Labrador</option>
+			<option value="NS">Nova Scotia</option>
+			<option value="ON">Ontario</option>
+			<option value="PE">Prince Edward Island</option>
+			<option value="QC">Quebec</option>
+			<option value="SK">Saskatchewan</option>
+			<option value="NT">Northwest Territories</option>
+			<option value="NU">Nunavut</option>
+			<option value="YT">Yukon</option>
+		</select></p> 
+
+		<p><label>Email:</label>
+		<input type="" name=""></p> 
+
+		<p><label>Phone Number:</label>
+		<input type="" name=""></p> 
+
+		<p><label>Add Designation:</label>
+		<select>
+			<option value=""></option>
+			<option value=""></option>
+		</select></p>
+
+		<p><input type="submit" name="submit" value="Add Student"></p>
+	</form>
+
+	<form name="" method="">
+		<h1>Search Student</h1>
+		<p><label>Search ID/Name:</label>
+		<input type="" name=""></p> 
+
+		<p><input type="submit" name="submit" value="Search Student"></p>
+	</form>
+
+	<form name="" method="">
+		<h1>Register Student In Section</h1>
+		<p><label>Search Student:</label>
+		<input type="" name=""></p> 
+		<p><input type="submit" name="submit" value="Search Student"></p>
+		
+		<p><label>Search Course:</label>
+		<select>
+			<option value=""></option>
+			<option value=""></option>
+		</select></p> 
+
+		<p><label>Search Section:</label>
+		<select>
+			<option value=""></option>
+			<option value=""></option>
+		</select></p>
+
+		<p><input type="submit" name="submit" value="Register Student"></p>
+	</form>
+</body>
+</html>
