2d43c2659b5fe4de00223ac727d8913e6221d02a
Mark Blaise <mblaise2@nait.ca>
2016-11-24 14:20:37 -0700

    class almost done - dummy info in schedule needed

--- include/header.php
+++ include/header.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Nait Dental School</title>
-	<link rel="stylesheet" href="/../styles/reset.css" type="text/css">
+	<link rel="stylesheet" href="../styles/reset.css" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Lato|Raleway" rel="stylesheet">
-	<link rel="stylesheet" href="/../styles/main.css" type="text/css">
+	<link rel="stylesheet" href="../styles/main.css" type="text/css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
@@ -13,32 +13,32 @@
 		<nav>
 			<ul class="main-nav">
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/appt_icon_96_96.png" alt="Appointment Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/appt_icon_96_96.png" alt="Appointment Icon"></a>
 					<h4 class="nav-label">Appointments</h4>
 				</li>
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/patient_icon_96_96.png" alt="Patient Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/patient_icon_96_96.png" alt="Patient Icon"></a>
 					<h4 class="nav-label">Patients</h4>
 				</li>
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/inventory_icon_96_96.png" alt="Inventory Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/inventory_icon_96_96.png" alt="Inventory Icon"></a>
 					<h4 class="nav-label">Inventory</h4>
 				</li>
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/class_icon_96_96.png" alt="Classes Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/class_icon_96_96.png" alt="Classes Icon"></a>
 					<h4 class="nav-label">Courses</h4>
 				</li>
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/student_icon_96_96.png" alt="Student Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/student_icon_96_96.png" alt="Student Icon"></a>
 					<h4 class="nav-label">Students</h4>
 				</li>
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/instructors_icon_96_96.png" alt="Instructor Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/instructors_icon_96_96.png" alt="Instructor Icon"></a>
 					<h4 class="nav-label">Instructors</h4>
 				</li>
 
 				<li class="group">
-					<a href="#" class="nav-link"><img class="nav-icon" src="/../assets/navicons/classroom_icon_96_96.png" alt="Classroom Icon"></a>
+					<a href="#" class="nav-link"><img class="nav-icon" src="../assets/navicons/classroom_icon_96_96.png" alt="Classroom Icon"></a>
 					<h4 class="nav-label">Classrooms</h4>
 				</li>
 			</ul>
--- screens/class.php
+++ screens/class.php
@@ -2,9 +2,9 @@
 <main class="main-content">
 	<!--Create Course(CourseID, CourseName, CourseDescription, CreditsAwarded)-->
 	<section class="grid group">
-		<h2>Create a Course</h3>
+		<h1>Create a Course</h1>
 		<hr>
-		<form class="">
+		<form class="grid group">
 			<div class="grid group">
 				<div class="col-2 form-section">
 					<label for="CourseID" >Course ID:</label>
@@ -17,253 +17,299 @@
 				</div>
 			</div>
 			<div class="grid group">
-				<div class="col-2 form-section">
+				<div class="col-3 form-section">
 					<label for="CourseDescription">Course Description:</label><br>
 					<textarea id="elementID" class="form-input form-textarea"></textarea>
 				</div>
 
-				<div class="col-2 form-section">
+				<div class="col-1 form-section">
 					<label for="Credits">Credits:</label>
 					<SELECT class="form-input form-ddl" name="Credits">
 						<option value="3">3</option>
 					</SELECT>
 				</div>
 			</div>
-			<input class="btn btn-submit" type="submit" value="Create Class">
+			<div class="grid group">
+				<div class="col-1 form-section">
+					<input class="btn btn-submit" type="submit" value="Create Class">
+				</div>
+			</div>
 		</form>
 	</section>
 
 	<!--Create Course Section(CourseSectionID, CourseID, SemesterID, InstructorID, SessionalInstructorID)-->
 	<section class="grid group">
-		<h2>Add Course Section</h3>
-		<form class="">
-			<label for="CourseID">Course ID:</label>
-			<SELECT class="form-input form-ddl" name="CourseID">
-				<option value="RootCanal101">Root Canals 101 - 1001</option>
-			</SELECT>
-		<br>
-			<label for="CourseID">Course Section ID:</label>
-			<input class="form-input" type="text" name="CourseID" readonly value="1018">
-		<br>
-			<label for="SemesterID">Semester:</label><br>
-			<input type="radio" name="Semester" value="Winter" checked> Winter<br>
-		    <input type="radio" name="Semester" value="Spring"> Spring<br>
-		    <input type="radio" name="Semester" value="Summer"> Summer<br>
-		    <input type="radio" name="Semester" value="Fall"> Fall
-	    <br>
-	    	<label for="SemesterYear">Year:</label>
-			<SELECT class="form-input form-ddl" name="SemesterYear">
-				<option value="2016">2016</option>
-			</SELECT>
-		<br>
-			<label for="Instructor">Instructor:</label>
-			<SELECT class="form-input form-ddl" name="Instructor">
-				<option value="52">Homer Simpson</option>
-			</SELECT>
-		<br>
-			<label for="InstructorSes">Sessional Instructor ID:</label>
-			<SELECT class="form-input form-ddl" name="InstructorSes">
-				<option value="67">Maggie Simpson</option>
-			</SELECT>
-		<br>
-			<input class="btn btn-submit" type="submit" value="Add Class Section">
+		<h1>Add Course Section</h1>
+		<form class="grid group">
+			<div class="grid group">
+				<div class="col-2 form-section">
+					<label for="CourseID">Course ID:</label>
+					<SELECT class="form-input form-ddl" name="CourseID">
+						<option value="RootCanal101">Root Canals 101 - 1001</option>
+					</SELECT>
+				</div>
+				<div class="col-2 form-section">
+					<label for="CourseID">Course Section ID:</label>
+					<input class="form-input" type="text" name="CourseID" readonly value="1018">
+				</div>
+			</div>
+
+			<div class="grid group">
+				<div class="col-2 form-section">
+					<label for="SemesterID">Semester:</label><br>
+					<input type="radio" name="Semester" value="Winter" checked> Winter<br>
+				    <input type="radio" name="Semester" value="Spring"> Spring<br>
+				    <input type="radio" name="Semester" value="Summer"> Summer<br>
+				    <input type="radio" name="Semester" value="Fall"> Fall
+			   </div>
+			   <div class="col-2 form-section">
+			    	<label for="SemesterYear">Year:</label>
+					<SELECT class="form-input form-ddl" name="SemesterYear">
+						<option value="2016">2016</option>
+					</SELECT>
+				</div>
+			</div>
+
+			<div class="grid group">
+				<div class="col-2 form-section">
+					<label for="Instructor">Instructor:</label>
+					<SELECT class="form-input form-ddl" name="Instructor">
+						<option value="52">Homer Simpson</option>
+					</SELECT>
+				</div>
+				<div class="col-2 form-section">
+					<label for="InstructorSes">Sessional Instructor ID:</label>
+					<SELECT class="form-input form-ddl" name="InstructorSes">
+						<option value="67">Maggie Simpson</option>
+					</SELECT>
+				</div>
+			</div>
+			<div class="grid group">
+				<div class="col-1 form-section">
+					<input class="btn btn-submit" type="submit" value="Add Class Section">
+				</div>
+			</div>
 		</form>
 	</section>
 
 	<!--Add Class to Room Schedule (ClassRoomScheduleID, ClassRoomID, CourseSectionID, Date)-->
 	<section class="grid group">
-		<h2>Assign Section to Room</h3>
+		<h1>Assign Section to Room</h1>
 		<br>
-		<form class="col-2">
-			<label for="RoomCourseID">Section:</label>
-			<SELECT class="form-input form-ddl" name="RoomCourseID">
-				<option value="1018">1018</option>
-			</SELECT>
-		<br>
-			<label for="CourseID">Course ID:</label>
-			<SELECT class="form-input form-ddl" name="CourseID">
-				<option value="RootCanal101">Root Canals 101 - 1001</option>
-			</SELECT>
-		<br>
-			<label for="RoomLocation">Location:</label>
-			<SELECT class="form-input form-ddl" name="RoomLocation">
-				<option value="MainDentalCampus">Main Dental Campus</option>
-			</SELECT>
-		<br>
-			<label for="Room">Room:</label>
-			<SELECT class="form-input form-ddl" name="Room">
-				<option value="202">QR-201</option>
-			</SELECT>
-		<br>
-			<input type="datetime-local">
-	    <br>
-			<input class="btn btn-submit" type="submit" value="Assign Section To Room">
+		<form class="grid group">
+			<div class="grid group">
+				<div class="col-2 form-section">
+					<label for="RoomLocation">Location:</label>
+					<SELECT class="form-input form-ddl" name="RoomLocation">
+						<option value="MainDentalCampus">Main Dental Campus</option>
+					</SELECT>
+				</div>
+				<div class="col-2 form-section">
+					<label for="Room">Room:</label>
+					<SELECT class="form-input form-ddl" name="Room">
+						<option value="202">QR-201</option>
+					</SELECT>
+				</div>
+			</div>
+			<div class="grid group">
+				<div class="col-1 form-section">
+					
+					<label for="RoomCourseID">Section:</label>
+					<SELECT class="form-input form-ddl" name="RoomCourseID">
+						<option value="1018">1018</option>
+					</SELECT>
+					
+					<div class="row-inside-row">
+						<label for="CourseID">Course ID:</label>
+						<SELECT class="form-input form-ddl" name="CourseID">
+							<option value="RootCanal101">Root Canals 101 - 1001</option>
+						</SELECT>
+					</div>
+					<div class="row-inside-row">
+						<label for="DayOfWeek">Day Of Week:</label>
+						<SELECT class="form-input form-ddl" name="DayOfWeek">
+							<option value="Mon">Monday</option>
+						</SELECT>
+					</div>
+					<div class="row-inside-row">
+						<label for="StartTime">Start Time:</label>
+						<SELECT class="form-input form-ddl" name="StartTime">
+							<option value="13">1:00pm</option>
+						</SELECT>
+					</div>
+				</div>
+				<div class="col-3 form-section">
+					<label for="schedule">Room Schedule</h4>
+					<table name="schedule" id="schedule">
+						<tr>
+							<th>Time</th>
+							<th>Sunday</th>
+							<th>Monday</th>
+							<th>Tuesday</th>
+							<th>Wednesday</th>
+							<th>Thursday</th>
+							<th>Friday</th>
+							<th>Saturday</th>
+						</tr>
+						<tr>
+							<td><strong>8:00am</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>9:00am</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>10:00am</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>11:00am</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>12:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>1:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>2:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>3:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>4:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>5:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>					
+						<tr>
+							<td><strong>6:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>7:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>8:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+						<tr>
+							<td><strong>9:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>									
+						<tr>
+							<td><strong>10:00pm</strong></td>
+							<td>Sunday</td>
+							<td>Monday</td>
+							<td>Tuesday</td>
+							<td>Wednesday</td>
+							<td>Thursday</td>
+							<td>Friday</td>
+							<td>Saturday</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="grid group">
+					<div class="col-1 form-section">
+						<input class="btn btn-submit" type="submit" value="Assign Section To Room">
+					</div>
+			</div>
 		</form>
-		<div class="col-2">
-			<h4>Room Schedule</h4>
-			<table>
-				<tr>
-					<th>Time</th>
-					<th>Sunday</th>
-					<th>Monday</th>
-					<th>Tuesday</th>
-					<th>Wednesday</th>
-					<th>Thursday</th>
-					<th>Friday</th>
-					<th>Saturday</th>
-				</tr>
-				<tr>
-					<td><strong>8:00am</strong></td>
-					<td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>9:00am</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>10:00am</strong
-					></td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>11:00am</strong></td>
-					<td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>12:00pm</strong
-					></td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>1:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>2:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>3:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>4:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>5:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>					
-				<tr>
-					<td><strong>6:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>7:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>8:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-				<tr>
-					<td><strong>9:00pm</strong>
-					</td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>									
-				<tr>
-					<td><strong>10:00pm</strong
-					></td><td><p>Sunday</p></td>
-					<td><p>Monday</p></td>
-					<td><p>Tuesday</p></td>
-					<td><p>Wednesday</p></td>
-					<td><p>Thursday</p></td>
-					<td><p>Friday</p></td>
-					<td><p>Saturday</p></td>
-				</tr>
-			</table>
-		</div>
 	</section>
 </main>
 <?php include('../include/footer.php') ?>
